05df754240cf32b95088c7dc1065cc9b3a959be2
yukyoung <have224@skuniv.ac.kr>
2023-12-22 17:26:57 +0900

    board page 수정

---
+++ src/main/java/com/kh/woofly/board/model/vo/FreeBoard.java
@@ -0,0 +1,26 @@
+package com.kh.woofly.board.model.vo;
+
+import java.sql.Clob;
+import java.sql.Date;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@ToString
+
+public class FreeBoard {
+	private int bNo;
+	private String bTitle;
+	private Clob bContent;
+	private int bCount;
+	private Date bCreateDate;
+	private String bStatus;
+	private String mbId;
+}
---
+++ src/main/java/com/kh/woofly/board/model/vo/LostBoard.java
@@ -0,0 +1,25 @@
+package com.kh.woofly.board.model.vo;
+
+import java.sql.Clob;
+import java.sql.Date;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@ToString
+public class LostBoard {
+	private int mNo;
+	private String mTitle;
+	private Clob mContent;
+	private int mCount;
+	private Date mCreateDate;
+	private String mStauts;
+	private String mbId;
+}
